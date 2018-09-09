5c997a53eef0bc63a6235f5bf115915b125e8670
shingalona <savithri0707@gmail.com>
2018-09-10 01:39:58 +0530

    added search funtion

--- src/app.jsx
+++ src/app.jsx
@@ -1,37 +1,64 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import {FormGroup,FormControl,InputGroup,Glyphicon} from 'react-bootstrap';
-class App extends Component{
-    render(){
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ""
+        }
+    }
+    componentDidMount(){
+    
+    }
+    search() {
+        console.log("this.state", this.state);
+        const BASE_URL = 'https://api.spotify.com/v1/search?';
+        const FETCH_URL = BASE_URL + 'q=' + this.state.query
+                                    +'&type=artist&limit=1';
+        console.log('FETCH_URL',FETCH_URL);
+    }
+
+    render() {
         return (
-        <div className ="App">
-            <div className="App-title">
-                Music Master
+            <div className="App">
+                <div className="App-title">
+                    Music Master
             </div>
-            <FormGroup>
-                <InputGroup>
-                    <FormControl
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl
                             type="text"
                             placeholder="Search for artists"
-                    /> 
-                    <InputGroup.Addon>
-                        <Glyphicon glyph="search"></Glyphicon>
-                    </InputGroup.Addon>
-                </InputGroup>
-            </FormGroup>
-            <div className="Profiles">
-            <div>Artist Picture</div>
-            <div>Artist Nmae</div>
-            </div>
-            <div className="Gallery">
-            Gallery
+                            query={this.state.query}
+                            onChange={event => { this.setState({ query: event.target.value }) }}
+                            onKeyPress={
+                                event => {
+                                    if (event.key === "Enter") {
+                                        this.search()
+                                    }
+                                }
+                            }
+
+                        />
+                        <InputGroup.Addon onClick={() => { this.search() }}>
+                            <Glyphicon glyph="search"></Glyphicon>
+                        </InputGroup.Addon>
+                    </InputGroup>
+                </FormGroup>
+                <div className="Profiles">
+                    <div>Artist Picture</div>
+                    <div>Artist Name</div>
+                </div>
+                <div className="Gallery">
+                    Gallery
             </div>
 
-            
-        </div> 
-    );
+
+            </div>
+        );
     }
-   
+
 
 }
 export default App;
