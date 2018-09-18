a61f58ba9db9c91786ef760005bde9127c063165
shingalona <savithri0707@gmail.com>
2018-09-18 19:49:07 +0530

    created webpage and added integrated with spotify API

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import Profile from './profile';
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            query: ""
+            query: "",
+            artist: null
         }
     }
-    componentDidMount(){
-    
+    componentDidMount() {
+        var url = 'http://localhost:3001/getAccessToken';
+        fetch(url)
+            .then(response => {
+                return response.json();
+            })
+            .then(res => {
+                console.log('token received', JSON.stringify(res));
+                this.setState({ accessToken: res.access_token });
+            })
+            .catch(error => console.error('Error', error));
+
     }
     search() {
+
         console.log("this.state", this.state);
         const BASE_URL = 'https://api.spotify.com/v1/search?';
-        const FETCH_URL = BASE_URL + 'q=' + this.state.query
-                                    +'&type=artist&limit=1';
-        console.log('FETCH_URL',FETCH_URL);
+        const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+        console.log('FETCH_URL', FETCH_URL);
+        //API call to spotify to get the author details
+        fetch(FETCH_URL, {
+            method: "GET",
+            headers: {
+                "Authorization": "Bearer " + this.state.accessToken
+            }
+        })
+            .then(res => {
+                return res.json();
+            })
+            .then(result => {
+                const artist = result.artists.items[0];
+                console.log('artists', artist);
+                this.setState({ artist: artist });
+                // console.log("searchResult",JSON.stringify(result));
+            })
+
+
+            .catch(error => console.log(error));
+
     }
 
     render() {
@@ -46,13 +78,20 @@ class App extends Component {
                         </InputGroup.Addon>
                     </InputGroup>
                 </FormGroup>
-                <div className="Profiles">
-                    <div>Artist Picture</div>
-                    <div>Artist Name</div>
-                </div>
-                <div className="Gallery">
-                    Gallery
-            </div>
+                {
+                    this.state.artist !== null
+                        ?
+                        <div>
+                            <Profile
+                                artist={this.state.artist}
+                            />
+                            <div className="Gallery">
+                                Gallery
+                    </div>
+                        </div>
+                        : <div></div>
+                }
+
 
 
             </div>
